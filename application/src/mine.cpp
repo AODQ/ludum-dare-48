ccd248feb1b032480a00eda14b374905b7c51d26
Joseph Durel <jadematrix.art@gmail.com>
2021-04-24 17:36:53 -0400

    Cellular random mine chasm with gradient in rock type

--- application/src/mine.cpp
+++ application/src/mine.cpp
@@ -15,6 +15,11 @@ namespace
     ld::RockGemType::Empty,
     10,
   };
+
+  constexpr float lerp(float n1, float n2, float f)
+  {
+    return (n2 * f) + (n1 * (1.f - f));
+  }
 }
 
 
@@ -27,16 +32,29 @@ ld::MineChasm ld::MineChasm::Initialize(
     columns,
     decltype(ld::MineChasm::rocks)(columns * rows, basicRock)
   };
-  auto noise = ::GenImagePerlinNoise(columns, rows, 0, 0, 10.f);
+  auto pn1 = ::GenImagePerlinNoise(columns, rows, 0, 0, 10.f);
+  auto cn1 = ::GenImageCellular(columns, rows, 5);
 
   for (std::size_t row = 0; row < rows; ++row) {
     for (std::size_t col = 0; col < columns; ++col) {
       const auto i = row * columns + col;
       auto& rock = self.rocks[i];
-      const auto nv = reinterpret_cast<const ::Color*>(noise.data)[i].r / 255.f;
+      auto nv = (
+          reinterpret_cast<const ::Color*>(pn1.data)[i].r / 256.f
+        + reinterpret_cast<const ::Color*>(cn1.data)[i].r / 256.f
+      ) / 2.f;
+
+      nv = (
+        (row < rows / 2)
+        ? lerp(0.f,  nv, (static_cast<float>(row             ) / (rows / 2)))
+        : lerp(nv , 1.f, (static_cast<float>(row - (rows / 2)) / (rows / 2)))
+      );
 
       rock.type = static_cast<ld::RockType>(
-        static_cast<std::size_t>(nv * Idx(RockTier::Size))
+        static_cast<std::size_t>(
+          nv
+          * Idx(RockTier::Size)
+        )
       );
 
       rock.tier = static_cast<ld::RockTier>(::GetRandomValue(
